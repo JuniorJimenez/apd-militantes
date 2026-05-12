014280817a6315c2f051b2c72bb8df6b04737379
JuniorJimenez <32951920+JuniorJimenez@users.noreply.github.com>
2026-05-12 12:18:55 -0400

    Update route.ts

--- app/api/militantes/route.ts
+++ app/api/militantes/route.ts
@@ -1,4 +1,5 @@
 // app/api/militantes/route.ts
+export const dynamic = 'force-dynamic';
 import { NextRequest, NextResponse }    from 'next/server'
 import { prisma }                       from '@/lib/prisma'
 import { rateLimit, LIMITS }            from '@/lib/security/rateLimit'
