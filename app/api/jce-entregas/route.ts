15a66edc54c85432421927f7a033a04b9bc463fe
JuniorJimenez <32951920+JuniorJimenez@users.noreply.github.com>
2026-05-12 12:09:22 -0400

    Update route.ts

--- app/api/jce-entregas/route.ts
+++ app/api/jce-entregas/route.ts
@@ -1,4 +1,5 @@
 // app/api/jce-entregas/route.ts
+export const dynamic = 'force-dynamic';
 import { NextRequest, NextResponse } from 'next/server'
 import { prisma }       from '@/lib/prisma'
 import { requireAdmin } from '@/lib/security/authGuard'
