90f243957a5259263d94d0477a77816f7ca19c5f
JuniorJimenez <32951920+JuniorJimenez@users.noreply.github.com>
2026-05-12 11:54:23 -0400

    Update route.ts

--- app/api/carnet/route.ts
+++ app/api/carnet/route.ts
@@ -1,5 +1,5 @@
 // app/api/carnet/route.ts
-
+export const dynamic = 'force-dynamic';
 import { NextRequest, NextResponse } from 'next/server'
 import { prisma }       from '@/lib/prisma'
 import { consultarJCE } from '@/lib/jce'
